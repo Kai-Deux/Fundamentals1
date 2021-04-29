9db69139b04dce6b3a5fd991de61c48afb83d2d7
Kai-Deux <78042229+Kai-Deux@users.noreply.github.com>
2021-04-29 12:40:22 +0200

    Add files via upload

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,10 @@
+let js = 'amazing';
+      if (js === 'amazing') alert('JavaScript is FUN!');
+
+      40+8+23-10; 
+      console.log(40+8+23-10);
+    
+const bill = 275;
+const tip = bill <= 300 && bill >= 50 ? bill * 0.15 :
+bill * 0.2;
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
